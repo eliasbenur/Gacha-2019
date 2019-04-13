5698e5bb3ffa5ee0615fafab754e088cc7a04c1d
AlexandreMagi <almagisson@orange.fr>
2019-04-14 00:26:12 +0200

    Movement v3

--- Assets/D_Alexandre/Scripts/Player.cs
+++ Assets/D_Alexandre/Scripts/Player.cs
@@ -19,22 +19,24 @@ public class Player : MonoBehaviour
     [Range(1f, 100f)]
     public float maxSpeed;
 
-    private readonly float velocityThreshold = 0.001f;
+    private readonly float velocityThreshold = 0.1f;
 
     public bool isJumping = false;
 
-    private Rigidbody rBody;
+    private Rigidbody2D rBody;
 
     // Start is called before the first frame update
     void Start()
     {
-        rBody = GetComponent<Rigidbody>();
+        rBody = GetComponent<Rigidbody2D>();
         rBody.freezeRotation = true ;
     }
 
     // Update is called once per frame
     void Update()
     {
+        this.ProceedJump();
+
         if (Input.GetKey(KeyCode.UpArrow))
         {
             this.isJumping = true;
@@ -47,45 +49,46 @@ public class Player : MonoBehaviour
         //Basic impulsion
         if (Input.GetKey(KeyCode.UpArrow) && rBody.velocity.y == 0 )
         {
-            rBody.velocity += Vector3.up * jumpVelocity;
+            rBody.velocity += Vector2.up * jumpVelocity;
         }
 
-       // if (rBody.velocity.x >= -this.velocityThreshold && rBody.velocity.x <= this.velocityThreshold && rBody.velocity.x != 0) rBody.velocity = new Vector3(rBody.velocity.x, 0, 0);
-       // if (rBody.velocity.y >= -this.velocityThreshold && rBody.velocity.y <= this.velocityThreshold && rBody.velocity.y != 0) rBody.velocity = new Vector3(0, rBody.velocity.y, 0);
+        if (rBody.velocity.x >= -this.velocityThreshold && rBody.velocity.x <= this.velocityThreshold && rBody.velocity.x != 0) rBody.velocity = new Vector2(rBody.velocity.x, 0);
+        if (rBody.velocity.y >= -this.velocityThreshold && rBody.velocity.y <= this.velocityThreshold && rBody.velocity.y != 0) rBody.velocity = new Vector2(0, rBody.velocity.y);
 
+        if (this.transform.position.y <= -10) this.transform.position = new Vector3();
 
         if (Input.GetKey(KeyCode.LeftArrow)) {
-            rBody.velocity -= new Vector3(1, 0, 0) * acceleration * Time.deltaTime;
+            rBody.velocity += Vector2.left * acceleration * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            rBody.velocity += new Vector3(1, 0, 0) * acceleration *  Time.deltaTime;
+            rBody.velocity += Vector2.right * acceleration *  Time.deltaTime;
            
         }
         
         //MaxSpeed
         if (Mathf.Abs(rBody.velocity.x) > maxSpeed)
         {
-            rBody.velocity = new Vector3(maxSpeed * (rBody.velocity.x > 0?1:-1), rBody.velocity.y, 0);
+            rBody.velocity = new Vector2(maxSpeed * (rBody.velocity.x > 0?1:-1), rBody.velocity.y);
 
         }
 
-        this.ProceedJump();
+
     }
 
     //Kinectic jump modulation
     private void ProceedJump()
     {
         //Debug.Log("Y = "+ rBody.velocity.y);
-
+        
         if (rBody.velocity.y < 0)
         {
-            rBody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            rBody.velocity += Vector2.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
         else if (rBody.velocity.y > 0 && !this.isJumping)
         {
-            rBody.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            rBody.velocity += Vector2.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
 }
