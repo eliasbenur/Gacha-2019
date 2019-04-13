63848bef34fc153bf0cfcc984b2aafac50dd73c8
Pierre <haro@netc.fr>
2019-04-13 22:13:45 +0200

    Dash proto

---
+++ Assets/D_Pierre/Dash.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+
+    public float dashTime;
+    public float dashSpeed;
+    public float startDashTime;
+    public int direction;
+    Rigidbody rb;
+    Transform tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(x, y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = rb.velocity + (Vector3)dir.normalized * dashSpeed;
+            Debug.Log(dir);
+        }
+
+        dashTime = dashTime - Time.deltaTime;
+
+    }
+}
---
+++ Assets/D_Pierre/LookJoy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookJoy : MonoBehaviour
+{
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction;
+        float inputY = Input.GetAxis("joyLookX");
+        float inputX = Input.GetAxis("joyLookY");
+
+        if (inputX != 0 || inputY != 0)
+        {
+            direction = new Vector3(inputY, -inputX, 0);
+            float step = speed * Time.deltaTime;
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, direction, step, 0.0f);
+            transform.rotation = Quaternion.LookRotation(newDir);
+        }
+    }
+}
