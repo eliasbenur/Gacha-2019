6b6603f7afabd88a396e38595c7ce0e014c7fbfd
Gregoire <gregoire.calonne@live.be>
2019-04-14 03:03:06 +0200

    Commit proto1

--- Assets/D_Romain/Scenes/Scripts/Plane.cs
+++ Assets/D_Romain/Scenes/Scripts/Plane.cs
@@ -5,21 +5,50 @@ using UnityEngine;
 public class Plane : MonoBehaviour
 {
     // Start is called before the first frame update
+    private List<Joycon> joycons;
+
+    public float[] stick;
+
+    public Vector3 gyro;
+
+    public Vector3 accel;
+
+    public int jc_ind = 0;
+
+    public Quaternion orientation;
+
+    public bool planeCanRotate = true;
+
     void Start()
     {
-        
+        joycons = JoyconManager.Instance.j;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Q))
+        if (joycons.Count > 0)
         {
-            transform.Rotate(0, transform.rotation.y + 5, 0);
+            Joycon j = joycons[jc_ind];
+            stick = j.GetStick();
+            if (planeCanRotate == true)
+            {
+                if (/*Droite*/j.GetStick()[1] < -0.6f)
+                {
+                    transform.Rotate(0, transform.rotation.y + (100 * Time.deltaTime), 0);
+                }
+                if (/*Gauche*/j.GetStick()[1] > 0.6f)
+                {
+                    transform.Rotate(0, transform.rotation.y - (100 * Time.deltaTime), 0);
+                }
+            }
+
         }
-        if(Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(0, transform.rotation.y - 5, 0);
-        }
+
+    }
+
+    public void StopPlaneRotation()
+    {
+        planeCanRotate = false;
     }
 }
--- Assets/D_Romain/Scenes/Scripts/Teeth.cs
+++ Assets/D_Romain/Scenes/Scripts/Teeth.cs
@@ -7,31 +7,55 @@ public class Teeth : MonoBehaviour
     bool snaping = false;
     public float snapTravelTime;
     // Start is called before the first frame update
+
+    private List<Joycon> joycons;
+
+    public float[] stick;
+
+    public Vector3 gyro;
+
+    public Vector3 accel;
+
+    public int jc_ind = 0;
+
+    public Quaternion orientation;
+
+    private bool CanShake;
+
+    public GameObject plane;
+
+
     void Start()
     {
+        joycons = JoyconManager.Instance.j;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (joycons.Count > 0)
         {
-            snaping = true;
+            CheckShake();
+            Joycon j = joycons[jc_ind];
+            if (CanShake == true)
+            {
+                snaping = true;
+            }
+
+            if (snaping)
+            {
+                Snap();
+            }
+
+
         }
-
-        if(snaping)
-        {
-            Snap();
-        }
-
-
     }
 
     void Snap()
     {
-      
-        if(transform.localScale.y < 8)
+        plane.GetComponent<Plane>().StopPlaneRotation();
+        if (transform.localScale.y < 8)
         {
             transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + Time.deltaTime*7*(1/snapTravelTime), transform.localScale.z);
         }
@@ -41,5 +65,23 @@ public class Teeth : MonoBehaviour
     {
         transform.localScale = new Vector3(1.5f,1, 1);
     }
-    
+    bool CheckShake()
+    {
+        Joycon j = joycons[jc_ind];
+        if (joycons.Count > 0)
+        {
+            if (j.GetAccel().y > 1.5f || j.GetAccel().x > 1.5f || j.GetAccel().z > 1.5f)
+            {
+                Debug.Log("SHAKE IT OFF !");
+                CanShake = true;
+                return true;
+            }
+        }
+        CanShake = false;
+        return false;
+    }
+
+
+
+
 }
