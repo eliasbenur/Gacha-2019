add1f1f216a9562cdba07c1d3f1a6d21a11d92f2
Elias <eliasbenur@hotmail.com>
2019-04-14 16:06:53 +0200

    Gen Ale 0.3v

--- Assets/Master/Scripts/GeneracionAle.cs
+++ Assets/Master/Scripts/GeneracionAle.cs
@@ -7,7 +7,7 @@ public class GeneracionAle : MonoBehaviour
 
     private int Size_Hori_Cube = 10;
     private int Max_Cubes = 7, Min_Cubes = 3;
-    private int Max_NumCubes = 4, Min_NumCubes = 2;
+    //private int Max_NumCubes = 4, Min_NumCubes = 2;
 
     public GameObject prefab_Platforme;
 
@@ -15,7 +15,6 @@ public class GeneracionAle : MonoBehaviour
     void Start()
     {
         GenerationProcedural();
-
     }
 
     void GenerationProcedural()
@@ -27,138 +26,175 @@ public class GeneracionAle : MonoBehaviour
             switch (x)
             {
                 case 0:
-                    x_pos = 5;
-                    y_pos = -4;
-                    Max_Cubes = 5; Min_Cubes = 1;
-                    Max_NumCubes = 3; Min_NumCubes = 1;
+                    x_pos = 12 - 2;
+                    y_pos = -8;
+                    Max_Cubes = 8; Min_Cubes = 6;
                     break;
                 case 1:
-                    x_pos = 7;
-                    y_pos = -2;
-                    Max_Cubes = 7; Min_Cubes = 5;
-                    Max_NumCubes = 4; Min_NumCubes = 2;
+                    x_pos = 16 - 2;
+                    y_pos = -4;
+                    Max_Cubes = 12; Min_Cubes = 8;
                     break;
                 case 2:
-                    x_pos = 10;
+                    x_pos = 20 - 2;
                     y_pos = 0;
-                    Max_Cubes = 9; Min_Cubes = 6;
-                    Max_NumCubes = 5; Min_NumCubes = 2;
+                    Max_Cubes = 16; Min_Cubes = 12;
                     break;
                 case 3:
-                    x_pos = 7;
-                    y_pos = 2;
-                    Max_Cubes = 7; Min_Cubes = 5;
-                    Max_NumCubes = 4; Min_NumCubes = 2;
+                    x_pos = 16 - 2;
+                    y_pos = 4;
+                    Max_Cubes = 12; Min_Cubes = 8;
                     break;
                 case 4:
-                    x_pos = 5;
-                    y_pos = 4;
-                    Max_Cubes = 5; Min_Cubes = 1;
-                    Max_NumCubes = 3; Min_NumCubes = 1;
+                    x_pos = 12 - 2;
+                    y_pos = 8;
+                    Max_Cubes = 8; Min_Cubes = 6;
                     break;
             }
 
             int Cubes = Random.Range(Min_Cubes, Max_Cubes - 1);
             int NumSpaces = x_pos - Cubes;
-            int NumCubes = Random.Range(Min_NumCubes, Max_NumCubes);
             int NumSpacesTmp = NumSpaces;
             int NumSpaceUsed = 0;
 
             int IniSpace = 0;
             if (x == 0 || x == 4)
             {
-                IniSpace = Random.Range(0, x_pos - Cubes);
+                IniSpace = Random.Range(2, x_pos - Cubes);
             }
             else
             {
-                IniSpace = 0;
+                IniSpace = 2;
             }
 
-
-
-            GameObject inst_tmp = Instantiate(prefab_Platforme, new Vector3(-(x_pos)/2, y_pos, 0) + new Vector3(IniSpace, 0, 0), Quaternion.identity);
-            inst_tmp.transform.parent = transform;
+            NumSpaceUsed += IniSpace;
 
             int Taille_inst = 0;
+            Taille_inst = Random.Range(2, Cubes);
+            Debug.Log(Taille_inst);
 
-            if (NumCubes == 1)
+            if (Taille_inst > 5)
             {
-                Taille_inst = Cubes;
-            }
-            else
-            {
-                Taille_inst = Random.Range(1, Cubes);
-                if (Taille_inst > 3)
-                {
-                    Taille_inst = 3;
-                }
+                Taille_inst = 5;
             }
 
-
-            inst_tmp.transform.localScale = new Vector3(Taille_inst, 0.5f, 1);
-            inst_tmp.transform.position += new Vector3((Taille_inst - 1) * 0.5f, 0, 0);
+            for (int y = 0; y < Taille_inst; y ++)
+            {
+                GameObject inst_tmp = Instantiate(prefab_Platforme, new Vector3(-(x_pos) / 2, y_pos, 0) + new Vector3(NumSpaceUsed, 0, 0), Quaternion.identity);
+                inst_tmp.transform.parent = transform;
+                NumSpaceUsed += 1;
+            }
 
             Cubes -= Taille_inst;
-            NumCubes--;
             NumSpaces -= IniSpace;
 
-            NumSpaceUsed = Taille_inst + IniSpace;
-            
-            while (Cubes > 0 && NumCubes > 0)
+            ////////////////////Right
+
+            Taille_inst = Random.Range(2, Cubes);
+
+            if (Taille_inst > 5)
+            {
+                Taille_inst = 5;
+            }
+
+            for (int y = 0; y < Taille_inst; y++)
+            {
+                GameObject inst_tmp = Instantiate(prefab_Platforme, new Vector3((x_pos) / 2, y_pos, 0) - new Vector3(y, 0, 0), Quaternion.identity);
+                inst_tmp.transform.parent = transform;
+            }
+
+            Cubes -= Taille_inst;
+
+            while (Cubes > 0)
             {
                 int Cubes_tmp = Random.Range(1, Cubes);
 
-                if ((NumCubes == 1 || Cubes_tmp == Cubes) && (x == 0 && x ==4))
+                if ((Cubes_tmp == Cubes) && (x == 0 || x ==4))
                 {
-                    inst_tmp = Instantiate(prefab_Platforme, new Vector3(-(x_pos/2), y_pos, 0) + new Vector3(x_pos , 0, 0), Quaternion.identity);
-                    inst_tmp.transform.parent = transform;
+
+                    IniSpace = NumSpaces;
+
+                    NumSpaceUsed += IniSpace;
 
                     Taille_inst = Cubes;
 
-                    inst_tmp.transform.localScale = new Vector3(Taille_inst, 0.5f, 1);
-                    inst_tmp.transform.position -= new Vector3((Taille_inst - 1) * 0.5f, 0, 0);
+                    for (int y = 0; y < Taille_inst; y++)
+                    {
+                        GameObject inst_tmp = Instantiate(prefab_Platforme, new Vector3(-(x_pos / 2), y_pos, 0) + new Vector3(NumSpaceUsed, 0, 0), Quaternion.identity);
+                        inst_tmp.transform.parent = transform;
+                        NumSpaceUsed += 1;
+                    }
 
                     Cubes -= Taille_inst;
-                    NumCubes--;
-                    NumSpaces -= IniSpace;
-                }else if ((NumCubes == 1 || Cubes_tmp == Cubes))
-                {
-                    inst_tmp = Instantiate(prefab_Platforme, new Vector3(-(x_pos / 2), y_pos, 0) + new Vector3(x_pos, 0, 0), Quaternion.identity);
-                    inst_tmp.transform.parent = transform;
-
-                    Taille_inst = Cubes;
-
-                    inst_tmp.transform.localScale = new Vector3(Taille_inst, 0.5f, 1);
-                    inst_tmp.transform.position -= new Vector3((Taille_inst - 1) * 0.5f, 0, 0);
-
-                    Cubes -= Taille_inst;
-                    NumCubes--;
                     NumSpaces -= IniSpace;
                 }
-                else
+                /*else if (Cubes_tmp == Cubes)
                 {
-                    IniSpace = Random.Range(1, NumSpacesTmp - NumSpaces);
 
-                    inst_tmp = Instantiate(prefab_Platforme, new Vector3(-(x_pos / 2), y_pos, 0) + new Vector3(NumSpaceUsed + IniSpace, 0, 0), Quaternion.identity);
-                    inst_tmp.transform.parent = transform;
+                    IniSpace = NumSpaces;
 
-                    if (NumCubes == 1)
+                    NumSpaceUsed += IniSpace;
+
+                    Taille_inst = Cubes;
+
+                    for (int y = 0; y < Taille_inst; y++)
                     {
-                        Taille_inst = Cubes;
+                        GameObject inst_tmp = Instantiate(prefab_Platforme, new Vector3(-(x_pos / 2), y_pos, 0) + new Vector3(NumSpaceUsed, 0, 0), Quaternion.identity);
+                        inst_tmp.transform.parent = transform;
+                        NumSpaceUsed += 1;
                     }
-                    else
-                    {
-                        Taille_inst = Random.Range(1, Cubes);
-                    }
-
-                    inst_tmp.transform.localScale = new Vector3(Taille_inst, 0.5f, 1);
-                    inst_tmp.transform.position += new Vector3(Taille_inst - 1, 0, 0);
 
                     Cubes -= Taille_inst;
-                    NumCubes--;
                     NumSpaces -= IniSpace;
 
-                    NumSpaceUsed = Taille_inst + IniSpace;
+                    Debug.Log("Hey");
+
+                }*/
+                else
+                {
+                    IniSpace = Random.Range(1, NumSpaces);
+                    if (IniSpace < 3)
+                    {
+                        if (NumSpaces >=3)
+                        {
+                            IniSpace = 3;
+                        }else if (NumSpaces >= 2)
+                        {
+                            IniSpace = 2;
+                        }
+                    }
+
+                    NumSpaceUsed += IniSpace;
+
+                    Taille_inst = Cubes_tmp;
+
+                    if (Taille_inst < 3)
+                    {
+                        if (Cubes >= 3)
+                        {
+                            Taille_inst = 3;
+                        }
+                        else if (Cubes >= 2)
+                        {
+                            Taille_inst = 2;
+                        }
+
+                    }
+
+                    if (Taille_inst > 8)
+                    {
+                        Taille_inst = 8;
+                    }
+
+                    for (int y = 0; y < Taille_inst; y++)
+                    {
+                        GameObject inst_tmp = Instantiate(prefab_Platforme, new Vector3(-(x_pos / 2), y_pos, 0) + new Vector3(NumSpaceUsed, 0, 0), Quaternion.identity);
+                        inst_tmp.transform.parent = transform;
+                        NumSpaceUsed += 1;
+                    }
+
+                    Cubes -= Taille_inst;
+                    NumSpaces -= IniSpace;
                 }
             }
         }
